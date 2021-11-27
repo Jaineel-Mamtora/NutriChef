a3a924faabd102be009cba4239484ea3f204f2a1
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-11-27 19:30:33 +0530

    added: Network Module to provide (~ to instantiate) Retrofit builder

---
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package com.trailblazing.modernfoodrecipesapp.di
+
+import com.trailblazing.modernfoodrecipesapp.Constants.Companion.BASE_URL
+import com.trailblazing.modernfoodrecipesapp.FoodRecipesApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConvertorFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): FoodRecipesApi {
+        return retrofit.create(FoodRecipesApi::class.java)
+    }
+
+
+}
