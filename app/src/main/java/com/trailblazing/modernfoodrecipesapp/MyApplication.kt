b1777467692f2de9add3638ad5697afae42ff0a2
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-11-27 19:16:46 +0530

    defined base application explicitly & attached it in AndroidManifest file

---
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.trailblazing.modernfoodrecipesapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+
+}
---
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/RemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.trailblazing.modernfoodrecipesapp
+
+class RemoteDataSource() {
+}
