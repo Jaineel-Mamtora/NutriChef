393dd3d471215996958fa077be1b53b8ecf4e434
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-11-27 19:43:22 +0530

    added: RemoteDataSource to consume data from Network Module

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/RemoteDataSource.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/RemoteDataSource.kt
@@ -1,4 +1,15 @@
 package com.trailblazing.modernfoodrecipesapp
 
-class RemoteDataSource() {
+import com.trailblazing.modernfoodrecipesapp.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
+
 }
