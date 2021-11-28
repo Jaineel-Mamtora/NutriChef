b7a6ae3c6e101f065f4d1a1de59238790b28d3d1
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-11-28 22:17:44 +0530

    added: LocalDataSource which injects (~instantiates) RecipesDao

---
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/data/LocalDataSource.kt
@@ -0,0 +1,20 @@
+package com.trailblazing.modernfoodrecipesapp.data
+
+import com.trailblazing.modernfoodrecipesapp.data.database.RecipesDao
+import com.trailblazing.modernfoodrecipesapp.data.database.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+    private val recipesDao: RecipesDao,
+) {
+
+    fun readDatabase(): Flow<List<RecipesEntity>> {
+        return recipesDao.readRecipes()
+    }
+
+    suspend fun insertRecipes(recipesEntity: RecipesEntity) {
+        recipesDao.insertRecipes(recipesEntity)
+    }
+
+}
