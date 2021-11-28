a24bd875c85b5e538d6da55da55e67d6723315ae
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-11-28 21:53:52 +0530

    added: RecipesTypeConverter to serialize & deserialize recipe data

---
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/RecipesTypeConverter.kt
@@ -0,0 +1,23 @@
+package com.trailblazing.modernfoodrecipesapp
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.trailblazing.modernfoodrecipesapp.models.FoodRecipe
+
+class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe): String {
+        return gson.toJson(foodRecipe)
+    }
+
+    @TypeConverter
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val listType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, listType)
+    }
+
+}
