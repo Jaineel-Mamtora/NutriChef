6f34316bbef7570bbd31447ac36bdb2ff48481d7
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-11-28 21:32:47 +0530

    added: RecipesEntity

---
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/RecipesEntity.kt
@@ -0,0 +1,16 @@
+package com.trailblazing.modernfoodrecipesapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.trailblazing.modernfoodrecipesapp.models.FoodRecipe
+import com.trailblazing.modernfoodrecipesapp.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+
+    @PrimaryKey(autoGenerate = false)
+    val id: Int = 0
+
+}
