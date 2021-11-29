65c1176666158ed27ee0cd97b4047031c7db47fe
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-11-29 16:44:33 +0530

    added: recipes_bottom_sheet UI

---
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -0,0 +1,19 @@
+package com.trailblazing.modernfoodrecipesapp.ui.fragments.recipes.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.trailblazing.modernfoodrecipesapp.R
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+    }
+}
