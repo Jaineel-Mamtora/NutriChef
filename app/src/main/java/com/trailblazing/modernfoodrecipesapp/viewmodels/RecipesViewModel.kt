a2e12604456eb08c93f3204c4aa46a904e3d0e6d
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-11-30 10:57:01 +0530

    modified: DataStoreRepository to store shared-preferences

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/viewmodels/RecipesViewModel.kt
@@ -2,7 +2,10 @@ package com.trailblazing.modernfoodrecipesapp.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import com.trailblazing.modernfoodrecipesapp.util.Constants
+import com.trailblazing.modernfoodrecipesapp.util.Constants.Companion.API_KEY
+import com.trailblazing.modernfoodrecipesapp.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.trailblazing.modernfoodrecipesapp.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.trailblazing.modernfoodrecipesapp.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import com.trailblazing.modernfoodrecipesapp.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import com.trailblazing.modernfoodrecipesapp.util.Constants.Companion.QUERY_API_KEY
 import com.trailblazing.modernfoodrecipesapp.util.Constants.Companion.QUERY_DIET
@@ -15,10 +18,10 @@ class RecipesViewModel(application: Application) : AndroidViewModel(application)
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
-        queries[QUERY_NUMBER] = "50"
-        queries[QUERY_API_KEY] = Constants.API_KEY
-        queries[QUERY_TYPE] = "main course"
-        queries[QUERY_DIET] = "gluten free"
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
+        queries[QUERY_API_KEY] = API_KEY
+        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
