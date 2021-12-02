a89cfeb9f5e547ce23ea749ad6e42d1cd47cedec
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-12-02 22:37:12 +0530

    added: functionality to send result object to DetailsActivity using safe-args

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/bindingadapters/RecipesRowBinding.kt
@@ -1,16 +1,35 @@
 package com.trailblazing.modernfoodrecipesapp.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.trailblazing.modernfoodrecipesapp.R
+import com.trailblazing.modernfoodrecipesapp.models.Result
+import com.trailblazing.modernfoodrecipesapp.ui.fragments.recipes.RecipesFragmentDirections
 
 class RecipesRowBinding {
 
     companion object {
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout: ConstraintLayout, result: Result) {
+            recipeRowLayout.setOnClickListener {
+                try {
+                    val action =
+                        RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipeRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("onRecipeClickListener", e.toString())
+                }
+            }
+        }
 
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
