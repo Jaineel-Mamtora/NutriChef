e607265750786cb9f194d6b1ec241ab0fa503890
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2021-12-02 22:18:41 +0530

    added: DetailsActivity UI

---
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.trailblazing.modernfoodrecipesapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.trailblazing.modernfoodrecipesapp.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
