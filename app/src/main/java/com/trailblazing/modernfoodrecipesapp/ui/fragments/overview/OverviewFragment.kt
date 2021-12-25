ecd422bf8b6f5957e07d3e30e1cfe4d3f5d428be
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2021-12-25 16:34:25 +0530

    added: OverviewFragment - Display Recipe Data

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/fragments/overview/OverviewFragment.kt
@@ -1,11 +1,15 @@
 package com.trailblazing.modernfoodrecipesapp.ui.fragments.overview
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import coil.load
 import com.trailblazing.modernfoodrecipesapp.R
+import com.trailblazing.modernfoodrecipesapp.models.Result
+import kotlinx.android.synthetic.main.fragment_overview.view.*
 
 class OverviewFragment : Fragment() {
 
@@ -14,7 +18,108 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        val view = inflater.inflate(R.layout.fragment_overview, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable("recipeBundle")
+
+        view.mainImageView.load(myBundle?.image)
+        view.titleTextView.text = myBundle?.title
+        view.likesTextView.text = myBundle?.aggregateLikes.toString()
+        view.timeTextView.text = myBundle?.readyInMinutes.toString()
+        view.summaryTextView.text = myBundle?.summary
+
+        if (myBundle?.vegetarian == true) {
+            view.vegetarianImageView.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.vegetarianTextView.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.vegan == true) {
+            view.veganImageView.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.veganTextView.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.glutenFree == true) {
+            view.glutenFreeImageView.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.glutenFreeTextView.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.dairyFree == true) {
+            view.diaryFreeImageView.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.dairyFreeTextView.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.veryHealthy == true) {
+            view.healthyImageView.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.healthyTextView.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.cheap == true) {
+            view.cheapImageView.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.cheapTextView.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        return view
     }
 
 }
