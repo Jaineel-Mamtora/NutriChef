d4012b28ea3584a302d16d1119a70009997e5d48
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2021-12-25 17:36:08 +0530

    modified: generalized RecipesDiffUtil implementation

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/util/RecipesDiffUtil.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/util/RecipesDiffUtil.kt
@@ -1,11 +1,10 @@
 package com.trailblazing.modernfoodrecipesapp.util
 
 import androidx.recyclerview.widget.DiffUtil
-import com.trailblazing.modernfoodrecipesapp.models.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ) : DiffUtil.Callback() {
     override fun getOldListSize(): Int {
         return oldList.size
