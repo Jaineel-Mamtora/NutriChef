8f2062b57beb26c266a4b5331d03b1d550412813
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2021-12-25 17:50:32 +0530

    modified: Create IngredientsAdapter class

---
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/adapters/IngredientsAdapter.kt
@@ -0,0 +1,54 @@
+package com.trailblazing.modernfoodrecipesapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.trailblazing.modernfoodrecipesapp.R
+import com.trailblazing.modernfoodrecipesapp.models.ExtendedIngredient
+import com.trailblazing.modernfoodrecipesapp.util.Constants.Companion.BASE_IMAGE_URL
+import com.trailblazing.modernfoodrecipesapp.util.RecipesDiffUtil
+import kotlinx.android.synthetic.main.ingredients_row_layout.view.*
+import java.util.*
+
+class IngredientsAdapter : RecyclerView.Adapter<IngredientsAdapter.MyViewHolder>() {
+
+    private var ingredientsList = emptyList<ExtendedIngredient>()
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.ingredients_row_layout, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.itemView.ingredient_imageView.load(BASE_IMAGE_URL + ingredientsList[position].image)
+        holder.itemView.ingredient_name.text = ingredientsList[position].name.replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(
+                Locale.getDefault()
+            ) else it.toString()
+        }
+        holder.itemView.ingredient_amount.text = ingredientsList[position].amount.toString()
+        holder.itemView.ingredient_unit.text = ingredientsList[position].unit
+        holder.itemView.ingredient_consistency.text = ingredientsList[position].consistency
+        holder.itemView.ingredient_original.text = ingredientsList[position].original
+
+    }
+
+    override fun getItemCount(): Int {
+        return ingredientsList.size
+    }
+
+    fun setData(newIngredients: List<ExtendedIngredient>) {
+        val ingredientsDiffUtil =
+            RecipesDiffUtil(ingredientsList, newIngredients)
+        val diffUtilResult = DiffUtil.calculateDiff(ingredientsDiffUtil)
+        ingredientsList = newIngredients
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
