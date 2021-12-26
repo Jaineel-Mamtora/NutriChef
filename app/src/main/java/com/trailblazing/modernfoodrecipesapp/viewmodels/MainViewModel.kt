464beb1a1400470470e5df642639d1a9622d9d54
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2021-12-26 23:36:48 +0530

    modified: MainViewModel class

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/viewmodels/MainViewModel.kt
@@ -7,6 +7,7 @@ import android.net.NetworkCapabilities
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
 import com.trailblazing.modernfoodrecipesapp.data.Repository
+import com.trailblazing.modernfoodrecipesapp.data.database.entities.FavoritesEntity
 import com.trailblazing.modernfoodrecipesapp.data.database.entities.RecipesEntity
 import com.trailblazing.modernfoodrecipesapp.models.FoodRecipe
 import com.trailblazing.modernfoodrecipesapp.util.NetworkResult
@@ -24,10 +25,31 @@ class MainViewModel @ViewModelInject constructor(
     val readRecipes: LiveData<List<RecipesEntity>> =
         repository.local.readRecipes().asLiveData() // converted Flow to LiveData
 
+    val readFavoriteRecipes: LiveData<List<FavoritesEntity>> =
+        repository.local.readFavoriteRecipes().asLiveData()
+
     private fun insertRecipes(recipesEntity: RecipesEntity) =
         viewModelScope.launch(Dispatchers.IO) {
             repository.local.insertRecipes(recipesEntity)
         }
+
+    private fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.local.insertFavoriteRecipes(favoritesEntity)
+        }
+    }
+
+    private fun deleteFavoriteRecipes(favoritesEntity: FavoritesEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.local.deleteFavoriteRecipe(favoritesEntity)
+        }
+    }
+
+    private fun deleteAllFavoriteRecipes() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.local.deleteAllFavoriteRecipes()
+        }
+    }
 
     /** RETROFIT */
 
