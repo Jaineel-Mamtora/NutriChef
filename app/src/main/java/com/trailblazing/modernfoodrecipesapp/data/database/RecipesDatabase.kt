d1137731dff0d895f2e7c20d521313ba608162bc
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2021-12-26 23:44:33 +0530

    modified: RecipesDatabase class

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/data/database/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package com.trailblazing.modernfoodrecipesapp.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.trailblazing.modernfoodrecipesapp.data.database.entities.FavoritesEntity
 import com.trailblazing.modernfoodrecipesapp.data.database.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
+    entities = [RecipesEntity::class, FavoritesEntity::class],
     version = 1,
     exportSchema = false,
 )
