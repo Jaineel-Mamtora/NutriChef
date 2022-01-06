3c8e2cdaa4e158401589dd26d777bc6854f0d578
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2022-01-06 11:15:35 +0530

    added: displaying Favorite Recipes

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -5,15 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.trailblazing.modernfoodrecipesapp.R
+import com.trailblazing.modernfoodrecipesapp.adapters.FavoriteRecipesAdapter
+import com.trailblazing.modernfoodrecipesapp.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_favorite_recipes.view.*
 
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
+
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+
+        setupRecyclerView(view.favorite_recipes_recyclerview)
+
+        mainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner, { favoritesEntity ->
+            mAdapter.setData(favoritesEntity)
+        })
+
+        return view;
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 }
