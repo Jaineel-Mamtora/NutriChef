8daea7d5273ee25babfd831de321a7431b7be33e
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2022-01-07 13:52:06 +0530

    added: FavoriteRecipesFragment - Single Click Listener

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/adapters/FavoriteRecipesAdapter.kt
@@ -2,11 +2,14 @@ package com.trailblazing.modernfoodrecipesapp.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.trailblazing.modernfoodrecipesapp.data.database.entities.FavoritesEntity
 import com.trailblazing.modernfoodrecipesapp.databinding.FavoriteRecipesRowLayoutBinding
+import com.trailblazing.modernfoodrecipesapp.ui.fragments.favorites.FavoriteRecipesFragmentDirections
 import com.trailblazing.modernfoodrecipesapp.util.RecipesDiffUtil
+import kotlinx.android.synthetic.main.favorite_recipes_row_layout.view.*
 
 class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
 
@@ -38,6 +41,18 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val selectedRecipe = favoriteRecipes[position]
         holder.bind(selectedRecipe)
+
+        /**
+         * Single Click Listener
+         */
+
+        holder.itemView.favorite_recipes_row_layout.setOnClickListener {
+            val action =
+                FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(
+                    selectedRecipe.result
+                )
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int {
