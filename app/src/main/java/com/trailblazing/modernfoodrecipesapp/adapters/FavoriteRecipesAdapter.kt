03abae696cf069d59e61acfd7742954efc1fa7d9
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2022-01-07 14:02:26 +0530

    added: FavoriteRecipesFragment - Long Click Listener

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/adapters/FavoriteRecipesAdapter.kt
@@ -1,17 +1,21 @@
 package com.trailblazing.modernfoodrecipesapp.adapters
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.*
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.trailblazing.modernfoodrecipesapp.R
 import com.trailblazing.modernfoodrecipesapp.data.database.entities.FavoritesEntity
 import com.trailblazing.modernfoodrecipesapp.databinding.FavoriteRecipesRowLayoutBinding
 import com.trailblazing.modernfoodrecipesapp.ui.fragments.favorites.FavoriteRecipesFragmentDirections
 import com.trailblazing.modernfoodrecipesapp.util.RecipesDiffUtil
 import kotlinx.android.synthetic.main.favorite_recipes_row_layout.view.*
 
-class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
+class FavoriteRecipesAdapter(
+    private val requireActivity: FragmentActivity,
+) : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>(),
+    ActionMode.Callback {
 
     private var favoriteRecipes = emptyList<FavoritesEntity>()
 
@@ -53,6 +57,16 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
                 )
             holder.itemView.findNavController().navigate(action)
         }
+
+        /**
+         * Long Click Listener
+         */
+
+        holder.itemView.favorite_recipes_row_layout.setOnLongClickListener {
+            requireActivity.startActionMode(this)
+
+            true
+        }
     }
 
     override fun getItemCount(): Int {
@@ -64,5 +78,22 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
         val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
         favoriteRecipes = newFavoriteRecipes
         diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+    override fun onCreateActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+        actionMode?.menuInflater?.inflate(R.menu.favorites_contextual_menu, menu)
+        return true
+    }
+
+    override fun onPrepareActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+        return true
+    }
+
+    override fun onActionItemClicked(actionMode: ActionMode?, menu: MenuItem?): Boolean {
+        return true
+    }
+
+    override fun onDestroyActionMode(actionMode: ActionMode?) {
+
     }
 }
