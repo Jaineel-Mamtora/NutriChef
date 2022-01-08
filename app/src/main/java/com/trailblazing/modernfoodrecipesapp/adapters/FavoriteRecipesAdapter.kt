148d335bffe1e0aa92d7fef4da879894a76f6d21
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2022-01-08 13:52:27 +0530

    added: Close Contextual Action Mode

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/adapters/FavoriteRecipesAdapter.kt
@@ -187,4 +187,10 @@ class FavoriteRecipesAdapter(
         ).setAction("Okay") {}
             .show()
     }
+
+    fun clearContextualActionMode() {
+        if (this::mActionMode.isInitialized) {
+            mActionMode.finish()
+        }
+    }
 }
--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -52,5 +52,6 @@ class FavoriteRecipesFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        mAdapter.clearContextualActionMode()
     }
 }
