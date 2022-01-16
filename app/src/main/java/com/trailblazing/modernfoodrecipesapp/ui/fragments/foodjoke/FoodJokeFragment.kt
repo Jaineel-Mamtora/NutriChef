08131304324222bf4ce9834fbdaafaf3ada1acb8
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2022-01-16 18:43:29 +0530

    added: Add Share Menu to FoodJokeFragment

--- app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/fragments/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/trailblazing/modernfoodrecipesapp/ui/fragments/foodjoke/FoodJokeFragment.kt
@@ -1,14 +1,14 @@
 package com.trailblazing.modernfoodrecipesapp.ui.fragments.foodjoke
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.trailblazing.modernfoodrecipesapp.R
 import com.trailblazing.modernfoodrecipesapp.databinding.FragmentFoodJokeBinding
 import com.trailblazing.modernfoodrecipesapp.util.ApiKey.Companion.API_KEY
 import com.trailblazing.modernfoodrecipesapp.util.NetworkResult
@@ -24,6 +24,8 @@ class FoodJokeFragment : Fragment() {
     private var _binding: FragmentFoodJokeBinding? = null
     private val binding get() = _binding!!
 
+    private var foodJoke = "No Food Joke"
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,11 +34,16 @@ class FoodJokeFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.mainViewModel = mainViewModel
 
+        setHasOptionsMenu(true)
+
         mainViewModel.getFoodJoke(API_KEY)
         mainViewModel.foodJokeResponse.observe(viewLifecycleOwner, { response ->
             when (response) {
                 is NetworkResult.Success -> {
                     binding.foodJokeTextview.text = response.data?.text
+                    if (response.data != null) {
+                        foodJoke = response.data.text
+                    }
                 }
                 is NetworkResult.Error -> {
                     loadDataFromCache()
@@ -55,11 +62,28 @@ class FoodJokeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.food_joke_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.share_food_joke_menu) {
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT, foodJoke)
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun loadDataFromCache() {
         lifecycleScope.launch {
             mainViewModel.readFoodJoke.observe(viewLifecycleOwner, { database ->
                 if (!database.isNullOrEmpty()) {
                     binding.foodJokeTextview.text = database[0].foodJoke.text
+                    foodJoke = database[0].foodJoke.text
                 }
             })
         }
